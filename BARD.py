a7c4abac9a834a72d5f759cae7a2074fc7d64b8e
filippoLonghi <100634210+filippoLonghi@users.noreply.github.com>
2025-12-19 13:54:05 +0100

    processing in pipeline
    versione con processing funzionantre nella pipeline

--- BARD.py
+++ BARD.py
@@ -168,7 +168,6 @@ def run_pipeline(
                 py, str(story_script),
                 "--segments", str(clap_out_path),
                 "--words", str(words),
-                print(words),
                 "--print_live",
             ],
             cwd=str(root_dir),  # same as your terminal usage
@@ -184,6 +183,47 @@ def run_pipeline(
         print(f"\nERRORE CRITICO: uno script è fallito (exit code {e.returncode}).")
     except Exception as e:
         print(f"\nErrore imprevisto: {e}")
+
+        
+
+    import time
+    from pythonosc import udp_client
+
+    import json
+
+    # Percorso al file JSON nella stessa cartella di questo script
+    json_path = Path(__file__).resolve().parent / "story.json"
+
+    with json_path.open("r", encoding="utf-8") as f:
+        data = json.load(f)
+
+
+    ip = "127.0.0.1"
+    port = 5005
+    client = udp_client.SimpleUDPClient(ip, port)
+
+    timing = chunk_s
+
+    time.sleep(10) # lo sto usando ora solo per dare il tempo di runnare su processing le cose così dopo vedo quando gli arrivano i dati
+    print(f"Invio dati a {ip}:{port}...")
+
+
+    client.send_message("/config/duration", timing) 
+    print(f"Inviata durata: {timing}s")
+
+    # 2. Invio i segmenti uno alla volta
+    # Pattern: /segment -> [categoria (int), storia (string)]
+    for item in data["fragments"]:
+        cat = str(item["mood"])
+        txt = str(item["text"])
+        client.send_message("/segment", [cat, txt])
+        print(f"Inviato segmento cat {txt}")
+        time.sleep(0.05) # Piccola pausa per non intasare la rete (buona pratica)
+
+    client.send_message("/start", [])
+
+    print("Trasmissione completata.")
+
 
 
 if __name__ == "__main__":
@@ -202,5 +242,3 @@ if __name__ == "__main__":
         build_labelbank=args.build_labelbank,
         force_labelbank=args.force_labelbank,
     )
-
-
